b01a8c4d127d8b355071efac6b219867a218d3b9
m-viridis <125144648+m-viridis@users.noreply.github.com>
2024-01-03 22:45:32 -0500

    Update python-intro.py
    included functions

--- python-intro.py
+++ python-intro.py
@@ -17,3 +17,26 @@ b = 58
 c = 74
 average_grade = (a + b + c) / 3
 print(average_grade)
+
+
+def C2F(degrees_c):
+  '''docstring comment'''
+  degrees_f = (9 / 5) * degrees_c * 32
+  return degrees_f
+
+print(C2F(100))
+help(C2F)
+
+def salestax_calc(price, tax_rate=0.13):
+  tax_amount = price * tax_rate
+  return tax_amount
+
+salestax_calc(5)
+
+def total_bill(price, tax_rate=0.13, tip_rate=0.2): #default values must come at the end
+  tax = price * tax_rate
+  tip = price * tip_rate
+  total = price + tax + tip
+  return total
+
+total_bill(tip_rate=0.22, price=100)

1d23c89b7fa5df6910325bdaff0539e37f40bec8
m-viridis <125144648+m-viridis@users.noreply.github.com>
2024-01-03 22:44:37 -0500

    Create python-intro.py

---
+++ python-intro.py
@@ -0,0 +1,19 @@
+8*2.5
+#integer division; rounds down
+10 // 3
+50 > 25
+
+month = 12
+day = 11
+day_of_week = "Monday"
+print (month, day)
+
+degrees_cel = 25
+degrees_fah = (9 / 5) * degrees_cel + 32
+print(degrees_fah)
+
+a = 98
+b = 58
+c = 74
+average_grade = (a + b + c) / 3
+print(average_grade)
